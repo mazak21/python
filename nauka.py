80323f41a6506577cb3cd84785ba7fe96bcf332e
mazak21 <41908182+mazak21@users.noreply.github.com>
2023-12-07 09:35:32 +0100

    new file:   .vscode/tasks.json 	new file:   gwiazdki.py 	new file:   nauka.py 	new file:   palindrome_check.py 	new file:   python_learning.py

---
+++ nauka.py
@@ -0,0 +1,35 @@
+#Nauka kodowania w pythonie
+a=1
+b=567
+c=12123
+z=1*b/c
+print('what\'s up')
+if a >= b:
+    print ("a >= than b")
+elif a<b:
+    print("papapapa")
+else:
+    print ("a < than b")
+
+
+print ("Dupa jest czarna")
+print ("Dupa jest czarna")
+print ("Dupa jest czarna")
+
+
+def dupawolowa(text,number):
+    
+    print(text, number-4)
+    print(text, number-2)
+    print(text, number-1)
+
+dupawolowa("jakas dupa wolowa numer:", 111)
+
+
+def add_ten_to_age(age):
+    new_age = age + 10
+    return new_age
+
+How_old_I_be = add_ten_to_age(3)
+
+print(How_old_I_be)
---
+++ palindrome_check.py
@@ -0,0 +1,8 @@
+x=input("enter any sequence")
+y=x[::-1]
+if x==y:
+    print("PALINDROME")
+else:
+    print("NOT PALINDROME")
+
+print("papa")
