6efa4c1ff57f6d5912c187d208c61579ada5cb38
mazak21 <41908182+mazak21@users.noreply.github.com>
2023-12-07 19:05:36 +0100

    modified:   nauka.py

--- nauka.py
+++ nauka.py
@@ -1,37 +1,5 @@
-#Nauka kodowania w pythonie
-a=1
-b=567
-c=12123
-z=1*b/c
-print('what\'s up')
-if a >= b:
-    print ("a >= than b")
-elif a<b:
-    print("papapapa")
-else:
-    print ("a < than b")
+# hello_world.py
 
+import PySimpleGUI as sg
 
-print ("Dupa jest czarna")
-print ("Dupa jest czarna")
-print ("Dupa jest czarna raz dwa trzy")
-
-
-def dupawolowa(text,number):
-    
-    print(text, number-4)
-    print(text, number-2)
-    print(text, number-1)
-
-dupawolowa("jakas dupa wolowa numer:", 111)
-print("papapapapa!!!")
-print("Tutaj będziemy robili zmiany")
-
-
-def add_ten_to_age(age):
-    new_age = age + 10
-    return new_age
-
-How_old_I_be = add_ten_to_age(3)
-
-print(How_old_I_be)
+sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
